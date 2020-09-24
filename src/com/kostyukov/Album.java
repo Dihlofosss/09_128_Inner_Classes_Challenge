2e2e0a4c889cac147710bf73e813dfb04799af8d
Dihlofosss <maxim.kostyukov@googlemail.com>
2020-09-24 21:14:47 +0200

    1st solution

--- src/com/kostyukov/Album.java
+++ src/com/kostyukov/Album.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 
 public class Album
 {
-	private ArrayList<Song> songs = new ArrayList<>();
+	//private ArrayList<Song> songs = new ArrayList<>();
+	private SongList songs;
 	private String albumName;
 	
 	public String getAlbumName()
@@ -14,41 +15,87 @@ public class Album
 	
 	public ArrayList<Song> getSongs()
 	{
-		return songs;
+		//return songs;
+		return songs.songList;
 	}
 	
 	public Album(String albumName)
 	{
 		this.albumName = albumName;
+		songs = new SongList();
 	}
 	
 	public void addSong(String songName, double songDuration)
 	{
-		if (getSong(songName) != null){
-			System.out.println("Song " + songName + " is already in the Album");
-			return;
-		}
-		addSong(new Song(songName, songDuration));
+		//if (getSong(songName) != null){
+		//	System.out.println("Song " + songName + " is already in the Album");
+		//	return;
+		//}
+		//addSong(new Song(songName, songDuration));
+		
+		songs.addSong(songName, songDuration);
 	}
 	
 	public void addSong(Song newSong)
 	{
-		songs.add(newSong);
+		//songs.add(newSong);
+		songs.addSong(newSong);
 	}
 	
 	public Song getSong(int songID)
 	{
-		return songs.get(songID);
+		//return songs.get(songID);
+		return songs.findSong(songID);
 	}
 	
 	public Song getSong(String songName)
 	{
-		for (int i = 0; i < songs.size(); i++)
+		//for (int i = 0; i < songs.size(); i++)
+		//{
+		//	if (songs.get(i).getTitle().equals(songName))
+		//		return songs.get(i);
+		//}
+		
+		return songs.findSong(songName);
+	}
+	
+	private class SongList
+	{
+		private ArrayList<Song> songList;
+		
+		public SongList()
 		{
-			if (songs.get(i).getTitle().equals(songName))
-				return songs.get(i);
+			this.songList = new ArrayList<>();
 		}
-		System.out.println("Song " + songName + " is not existing");
-		return null;
+		
+		public void addSong(String songName, double songDuration)
+		{
+			if (findSong(songName) != null){
+				System.out.println("Song " + songName + " is already in the Album");
+				return;
+			}
+			addSong(new Song(songName, songDuration));
+		}
+		
+		public void addSong(Song song)
+		{
+			songList.add(song);
+		}
+		
+		public Song findSong(String song)
+		{
+			for (int i = 0; i < songList.size(); i++)
+			{
+				if (songList.get(i).getTitle().equals(song))
+					return songList.get(i);
+			}
+			System.out.println("Song " + song + " is not existing");
+			return null;
+		}
+		
+		public Song findSong(int songID)
+		{
+			return songList.get(songID);
+		}
 	}
 }
